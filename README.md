cfad2c867b0eed46d7ba6bf5b84dec5a78cda7e8
Michael Tanaya <m.tanaya@prescribe-digital.com>
2024-02-26 22:14:08 +0800

    Comparing two object using Equatable package

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-# blocms
+BLOCSM TUTURIAL REMINDERS:
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+WARNING:
+1. Never mutate existing states
+    - you can access the existing state but never modify
+2. Don't forget to mention your correct PROPS variable
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:blocms/logic/cubit/counter_cubit.dart';
 import 'package:blocms/logic/cubit/internet_cubit.dart';
 import 'package:blocms/router/app_rounter.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
+class MyClass extends Equatable {
+  final int value;
+
+  MyClass({required this.value});
+
+  @override
+  List<Object?> get props => [];
+}
+
 void main() {
-  runApp(MyApp(
-    appRounter: AppRounter(),
-    connectivity: Connectivity(),
-  ));
+  final a = MyClass(value: 1);
+  final b = MyClass(value: 2);
+
+  debugPrint("${a == b}");
+  debugPrint("${a == a}");
+  debugPrint("${b == b}");
+  // runApp(MyApp(
+  //   appRounter: AppRounter(),
+  //   connectivity: Connectivity(),
+  // ));
 }
 
 class MyApp extends StatelessWidget {
